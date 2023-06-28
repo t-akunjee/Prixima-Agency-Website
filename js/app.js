80b209b09f1815c6bf62d114b018af2dbe2ab874
t-akunjee <mtrakunjee@gmail.com>
2023-06-28 13:20:13 +0600

    Hero Slider Completed Successfully

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        smartSpeed:500,
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+    //Revolution Slider
+});
